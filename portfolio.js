36139c894130863ec60005c8ca82f05222e3c289
Clement OLIVIER <97695718+Clem-dev15@users.noreply.github.com>
2026-01-06 09:21:17 +0100

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  toggleButton.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
-});
